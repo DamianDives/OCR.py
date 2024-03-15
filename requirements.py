80eb934feb680b54289c34320dd7acc04439195d
Damian <163416644+DamianDives@users.noreply.github.com>
2024-03-15 09:23:58 +0530

    Create requirements.py

---
+++ requirements.py
@@ -0,0 +1,7 @@
+#REQUIREMENTS
+pip install pillow
+pip install opencv-python
+pip install numpy
+pip install pytesseract
+pip install tesseract
+#TESSERACT IS AN OCR TOOL WHICH CAN RECOGNIZE MORE THAN 100 LANGUAGES
